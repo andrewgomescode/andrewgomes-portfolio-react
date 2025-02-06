2dbb06593e55996ce6dc85682998d88346fd0768
andrewgomescode <andrewgomes.brc@gmail.com>
2025-02-06 15:23:41 -0300

    Atualiza o idioma do HTML para português e remove a configuração de animação do InfiniteSlider no Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,18 +12,6 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-
-      animation: {
-        ["infinite-slider"]: "infiniteSlider 20s linear infinite",
-      },
-      keyframes: {
-        infiniteSlider: {
-          "0%": { transform: "translateX(0)" },
-          "100%": {
-            transform: "translateX(-100%)",
-          },
-        },
-      },
     },
   },
   plugins: [],
