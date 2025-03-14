14ad7c63597f6ecc9996ad162c22fc79b9f437df
andrewgomescode <andrewgomes.brc@gmail.com>
2025-03-14 14:43:42 -0300

    Add new project "OVERCUB - Serviços de Boost" to data_projetos and update project IDs

--- src/data/data_projetos.js
+++ src/data/data_projetos.js
@@ -7,11 +7,20 @@ import portfolio from "/public/portfolio.png";
 import restaurante from "/public/restaurante.jpg";
 import sorveteria from "/public/sorveteria.jpg";
 import listaDeCompras from "/public/listaDeCompras.png";
+import overcub from "/public/overcub.png";
 
 
 export const data_projetos = [
   {
     id: 1,
+    nome: "OVERCUB - Serviços de Boost",
+    imagem: overcub,
+    descricao: "Aplicação Web API REST com registro, login e cadastro, um dashboard robusto, chat, sistema de ticket, criações automatizadas de produtos, notificações, etc. Inúmeras funcionalidades. !Projeto em desenvolvimento!  . CRUD, utilizando React, NextJS, Prisma e PostgreSQL.",
+    url: "https://proj-lista-de-compras.vercel.app/",
+    tecnologias: ["Html", "Css", "Javascript", "Typescript", "Tailwind", "React", "Next", "Prisma", "PostgreSQL", "CRUD"],
+  },
+  {
+    id: 2,
     nome: "App - Lista de Compras",
     imagem: listaDeCompras,
     descricao: "Aplicação web API REST com registro, login e cadastro de itens, para criar e gerenciar listas de compras com recursos de CRUD, utilizando React, NextJS, Prisma e PostgreSQL.",
@@ -19,7 +28,7 @@ export const data_projetos = [
     tecnologias: ["Javascript", "Typescript", "Tailwind", "React", "Next", "Prisma", "PostgreSQL", "CRUD"],
   },
   {
-    id: 2,
+    id: 3,
     nome: "(Este Site) - Portfólio",
     imagem: portfolio,
     descricao: "Portfólio feito em React e utilizando NextJS. Estilizado com Tailwind. Feito em 7 dias.",
@@ -28,7 +37,7 @@ export const data_projetos = [
   },
 
   {
-    id: 3,
+    id: 4,
     nome: "Website Formulário",
     imagem: forms,
     descricao: "Projeto de uma interface formulário, utilizando algumas bibliotecas para UX/UI. Estilização feita com Tailwind.",
@@ -37,7 +46,7 @@ export const data_projetos = [
   },
 
   {
-    id: 4,
+    id: 5,
     nome: "Restaurante - Cardápio Digital",
     imagem: restaurante,
     descricao: "Cardápio Digital para um restaurante fictício. Utilizando ferramentas de estado do React.",
@@ -46,7 +55,7 @@ export const data_projetos = [
   },
 
   {
-    id: 5,
+    id: 6,
     nome: "Ignite Feed - Rede Social",
     imagem: ignite,
     descricao: "Um aplicativo simulando uma rede social, feito em React e utilizando Vite. Estilização feita via CSS.",
@@ -55,7 +64,7 @@ export const data_projetos = [
   },
 
   {
-    id: 6,
+    id: 7,
     nome: "Academia Evolve",
     imagem: academia,
     descricao: "Projeto Simples feito utilizando estilização base o Bootstrap. O projeto foi um desafio e teve um prazo de algumas horas para ser entregue.",
@@ -63,7 +72,7 @@ export const data_projetos = [
     tecnologias: ["Html", "Css", "Bootstrap", "Javascript"],
   },
   {
-    id: 7,
+    id: 8,
     nome: "Manhattan - Coffee House",
     imagem: parallax,
     descricao: "Um website feito para praticar a técnica 'Parallax' utilizando CSS puro.",
@@ -72,7 +81,7 @@ export const data_projetos = [
   },
 
   {
-    id: 8,
+    id: 9,
     nome: "Agência Digital",
     imagem: agencia,
     descricao: "Website landing page feito com React, utilizando useState para troca de temas. Primeiro projeto dentro do curso da DevMedia utilizando useState.",
@@ -81,7 +90,7 @@ export const data_projetos = [
   },
 
   {
-    id: 9,
+    id: 10,
     nome: "Sorveteria",
     imagem: sorveteria,
     descricao: "Site de uma sorveteria, feito em React. Um dos primeiros projetos feito em React dentro do curso da DevMedia.",
